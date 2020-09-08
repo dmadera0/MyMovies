f93d7ddbf8102d4f17d741c8c14bc34a0c20a35a
sanjeevyg <yogi.sjv@gmail.com>
2020-09-08 15:28:38 -0600

    controller methods created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,22 @@
 class MoviesController < ApplicationController
+    def index 
+        @movies = Movie.all 
+        render json: @movies
+    end
+
+    def show  
+        @movie = Movie.find(params[:id])
+        render json: @movie
+    end
+
+    def create 
+        @movie = Movie.create(
+            title: params[:title], 
+            year: params[:year],
+            genre: params[:genre],
+            plot: params[:plot]
+            )
+        render json: "movie created"
+    end
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def index 
+        @users = User.all 
+        render json: @users
+    end
+
+    def show  
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create 
+        @user= User.create(
+            name: params[:name]
+        )
+        render json: "user created"
+    end
 end
